1db7eace11a2d315aba083afb17c885958bf6905
Zarif <zarifraja19@gmail.com>
2023-05-03 05:23:01 +0600

    3

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors')
-const homeCard =require('./Data/HomeCard.json');
+const cors = require("cors");
+const homeCard = require("./Data/HomeCard.json");
 
-app.use(cors())
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get('/homeCard',(req,res) =>{
-    res.send(homeCard);
+app.get("/homeCard", (req, res) => {
+  res.send(homeCard);
+});
+app.get("/:id", (req, res) =>{
+  const id = req.params.id;
+  console.log(id);
+  const viewRecipes = homeCard.find(n=> n.id == id);
+  res.send(viewRecipes)
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
