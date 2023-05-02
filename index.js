c1c484cdf3b77d047d8a542223a0d8044f51bd11
Zarif <zarifraja19@gmail.com>
2023-05-02 16:10:16 +0600

    2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require('cors')
 const homeCard =require('./Data/HomeCard.json');
 
+app.use(cors())
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
